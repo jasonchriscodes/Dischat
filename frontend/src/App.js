9879b3ead4eb0201a42be3d8ab3bc822cc1a66d7
Jason Christian <70122753+jasonchriscodes@users.noreply.github.com>
2022-12-20 15:36:12 +1300

    chore: add 3 discord section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,21 @@
 import "./App.css";
+import Discover from "./components/Discover";
+import Groups from "./components/Groups";
+import MainComponent from "./components/MainComponent";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="flex">
+      {/* Friends + Groups */}
+      <Groups />
+      {/* Discover */}
+      <Discover />
+      {/* Main Content */}
+      <MainComponent />
+      {/* Hero */}
+      {/* Featured */}
+      <div className="w-full h-screen bg-[#363940]"></div>
+    </div>
+  );
 }
 export default App;
---
+++ frontend/src/components/MainComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainComponent = () => {
+  return <div className="bg-[#363940]">MainComponent</div>;
+};
+
+export default MainComponent;
