9598846870fb45e2b6452d35ae974af47d4e3cf8
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-01 23:44:17 +1300

    fix: search box sizing

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -15,12 +15,12 @@ const Hero = () => {
       {/* Search Box */}
       <div className="relative z-10 flex flex-col items-center justify-end h-full font-bold text-white pb-4">
         <p className="text-[22px] font-black">Find your community on Discord</p>
-        <p className="pb-2">
+        <p className="pb-2 text-[15px]">
           From gaming, to music, to study, there's a place for you.{" "}
         </p>
         <input
           type="search"
-          className="rounded-[.3rem] pl-3 placeholder: text-[14px] placeholder:text-gray-600 outline-0"
+          className="rounded-[.3rem] pl-3 placeholder: text-[14px] placeholder:text-gray-600 outline-0 w-full max-w-[70vw] mb-4"
           placeholder="Explore servers"
         />
       </div>
