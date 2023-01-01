fdb1b33f6b82f26f859bc52f19c56825ed3ace09
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-01 23:41:44 +1300

    feat: add search box

--- frontend/src/components/Discover.js
+++ frontend/src/components/Discover.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Discover = () => {
-  return <div className="hidden sm:flex bg-[#303136]">Discover</div>;
+  // return <div className="hidden sm:flex bg-[#303136]">Discover</div>;
 };
 
 export default Discover;
--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -3,17 +3,27 @@ import hero from "./assets/hero.jpg";
 
 const Hero = () => {
   return (
-    <div className="relative h-[6rem] mx-2 mt-2 rounded-[.3rem]">
+    <div className="relative h-[17rem] mx-8 mt-6   rounded-[.3rem]">
       {/* Background Image */}
       <div className="absolute flex w-full">
         <img
           src={hero}
-          className="object-cover w-full rounded-[.3rem] h-[6rem]"
+          className="object-cover w-full rounded-[.3rem] h-[17rem]"
           alt="hero"
         />
       </div>
       {/* Search Box */}
-      <div className="relative z-10">hi</div>
+      <div className="relative z-10 flex flex-col items-center justify-end h-full font-bold text-white pb-4">
+        <p className="text-[22px] font-black">Find your community on Discord</p>
+        <p className="pb-2">
+          From gaming, to music, to study, there's a place for you.{" "}
+        </p>
+        <input
+          type="search"
+          className="rounded-[.3rem] pl-3 placeholder: text-[14px] placeholder:text-gray-600 outline-0"
+          placeholder="Explore servers"
+        />
+      </div>
     </div>
   );
 };
