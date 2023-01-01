3b6e3cf9e671f3aae5bc4ed6f5fd3308dacfd4b7
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-02 00:41:15 +1300

    chore: change background image

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,23 +1,22 @@
 import React from "react";
-import hero from "./assets/hero.jpg";
+import hero from "./assets/hero2.webp";
 import { FiSearch } from "react-icons/fi";
 
 const Hero = () => {
   return (
-    <div className="relative h-[17rem] mx-8 mt-6   rounded-[.3rem] z-10">
+    <div className="relative h-[15rem] mx-8 mt-6   rounded-[.3rem] z-10">
       {/* Background Image */}
       <div className="absolute flex w-full">
         <img
           src={hero}
-          className="object-cover w-full rounded-[.3rem] h-[17rem]"
+          className="object-cover w-full rounded-[.3rem] h-[15rem]"
           alt="hero"
         />
       </div>
       {/* Overlay */}
-      <div className="absolute flex w-full h-full bg-black/20 z-10">
-      </div>
+      <div className="absolute flex w-full h-full bg-black/23 z-10"></div>
       {/* Search Box */}
-      <div className="relative z-20 flex flex-col items-center justify-end h-full font-bold text-white pb-0">
+      <div className="relative z-20 flex flex-col items-center justify-center h-full font-bold text-white pb-0">
         <p className="text-[22px] font-black">Find your community on Discord</p>
         <p className="pb-2 text-[15px]">
           From gaming, to music, to study, there's a place for you.{" "}
@@ -25,7 +24,7 @@ const Hero = () => {
         <div className="relative flex items-center mb-4">
           <input
             type="search"
-            className="rounded-[.3rem] pl-3 placeholder: text-[14px] placeholder:text-gray-600 outline-0 w-full max-w-[70vw] py-1.5"
+            className="rounded-[.3rem] pl-3 placeholder: text-[14px] placeholder:text-gray-600 outline-0 w-full max-w-[70vw] py-1.5 w-[25rem]"
             placeholder="Explore servers"
           />
           <FiSearch className="absolute text-black right-2" />
