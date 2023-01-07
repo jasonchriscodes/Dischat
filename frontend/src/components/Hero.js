d51ea8a57e1691ffffb4ad54e8102a23776bc13f
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-07 15:34:37 +1300

    chore: add adjustment to mobile view

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -17,14 +17,14 @@ const Hero = () => {
       <div className="absolute flex w-full h-full bg-black/23 z-10"></div>
       {/* Search Box */}
       <div className="relative z-20 flex flex-col items-center justify-center h-full font-bold text-white pb-0 space-y-2">
-        <p className="text-[20px] font-black">Find your community on Discord</p>
-        <p className="pb-2 text-[14px]">
+        <p className="text-[19px] font-black">Find your community on Discord</p>
+        <p className="pb-2 text-[13.5px] text-white/70">
           From gaming, to music, to study, there's a place for you.{" "}
         </p>
         <div className="relative flex items-center mb-4">
           <input
             type="search"
-            className="rounded-[.3rem] pl-3 placeholder: text-[14px] placeholder:text-gray-600 outline-0 w-full max-w-[70vw] py-1.5 w-[25rem]"
+            className="rounded-[.3rem] pl-3 placeholder: text-[14px] placeholder:text-gray-500 outline-0 w-full max-w-[70vw] py-2 w-[25rem]"
             placeholder="Explore servers"
           />
           <FiSearch className="absolute text-black right-2" />
