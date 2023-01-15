e442e2e21462f8a20dccd88d32b6ea83d9f1b019
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-16 09:42:29 +1300

    chore: add profile picture

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -13,6 +13,13 @@ const Card = ({ size, description, title, profile, bg }) => {
       </div>
       <div className="bg-[#292B2F] relative">
         {/* Profile Pic */}
+        <div className="absolute -top-8 left-4 w-16 h-16 flex rounded-[0.4rem]">
+          <img
+            className="object-cover rounded-[0.4rem]"
+            src={profile}
+            alt="cover area"
+          />
+        </div>
         {/* Title */}
         {/* Description */}
         {/* Size */}
