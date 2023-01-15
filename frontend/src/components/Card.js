d7d62fc0ea409fe2946e5f6c8a05475c0603a159
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-16 09:42:15 +1300

    chore: add banner area

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,7 +1,24 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ size, description, title, profile, bg }) => {
+  return (
+    <div className="w-1/2">
+      {/* Banner  */}
+      <div className="h-52 flex">
+        <img
+          src={bg}
+          className="rounded-t-[1.5rem] object-cover"
+          alt="banner area"
+        />
+      </div>
+      <div className="bg-[#292B2F] relative">
+        {/* Profile Pic */}
+        {/* Title */}
+        {/* Description */}
+        {/* Size */}
+      </div>
+    </div>
+  );
 };
 
 export default Card;
