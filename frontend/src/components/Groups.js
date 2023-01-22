695976907ef1cfa4c41515d47ee90edc83d3242b
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-23 10:38:18 +1300

    chore: add discord icon

--- frontend/src/components/Groups.js
+++ frontend/src/components/Groups.js
@@ -3,6 +3,7 @@ import img1 from "../components/assets/img1.png";
 import img2 from "../components/assets/img2.png";
 import img3 from "../components/assets/img3.png";
 import img4 from "../components/assets/img4.png";
+import dc from "../components/assets/dc.png";
 import { FiPlus } from "react-icons/fi";
 import { AiFillCompass } from "react-icons/ai";
 
@@ -11,6 +12,14 @@ const Groups = () => {
   return (
     <div className="hidden sm:flex bg-[#212226] w-[6rem] shrink-0 h-screen sticky pt-6">
       <div className="flex flex-col items-center w-full space-y-4">
+        {/* Discord */}
+        <div className="bg-[#36393f] p-1 rounded-[1.3rem]">
+          <img
+            src={dc}
+            className="w-12 h-12 rounded-full text-white brightness-[80%]"
+            alt="group-logo"
+          />
+        </div>
         {/* Compass */}
         <div className="bg-[#7289dc] p-3 rounded-[1.3rem]">
           <AiFillCompass
@@ -20,15 +29,18 @@ const Groups = () => {
           />
         </div>
         {/* Groups */}
-        {groups.map((group) => (
-          <div className="w-14 h-14 flex">
-            <img
-              src={group}
-              className="object-cover rounded-full"
-              alt="group-logo"
-            />
-          </div>
-        ))}
+        <div className="space-y-2 pt-4">
+          {groups.map((group) => (
+            <div className="w-14 h-14 flex">
+              <img
+                src={group}
+                className="object-cover rounded-full"
+                alt="group-logo"
+              />
+            </div>
+          ))}
+        </div>
+
         {/* Plus */}
         <div className="bg-[#36393f] p-4 rounded-full">
           <FiPlus
