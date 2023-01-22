3b730927fa7a4277d39b75b3e728a5d68640b72b
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-23 10:18:26 +1300

    chore: add Group icons

--- frontend/src/components/Groups.js
+++ frontend/src/components/Groups.js
@@ -1,9 +1,25 @@
 import React from "react";
+import img1 from "../components/assets/img1.png";
+import img2 from "../components/assets/img2.png";
+import img3 from "../components/assets/img3.png";
+import img4 from "../components/assets/img4.png";
 
 const Groups = () => {
+  const groups = [img1, img2, img3, img4, img1];
   return (
-    <div className="text-white hidden sm:flex bg-[#212226] w-[6rem] shrink-0">
-      Groups
+    <div className="hidden sm:flex bg-[#212226] w-[6rem] shrink-0 h-screen sticky">
+      {/* Groups */}
+      <div className="flex flex-col items-center w-full space-y-4">
+        {groups.map((group) => (
+          <div className="w-14 h-14 flex ">
+            <img
+              src={group}
+              className="object-cover rounded-full"
+              alt="group-logo"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
